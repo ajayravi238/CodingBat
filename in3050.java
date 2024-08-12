534fc733b44ee4ae985a62f7694dc71ddf3875b5
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-08-12 11:18:21 +0530

    Create in3050

---
+++ in3050.java
@@ -0,0 +1,11 @@
+public boolean in3050(int a, int b) {
+  if((a>29 && a<41) && (b>29 && b<41)) {
+    return true;
+  }
+  else if((a>39 && a<51) && (b>39 && b<51)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
