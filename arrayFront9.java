8ae5a00c8bc8148104bde211a05c683681c6eca5
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-08-21 18:48:26 +0530

    Create arrayFront9

---
+++ arrayFront9.java
@@ -0,0 +1,16 @@
+public boolean arrayFront9(int[] nums) {
+  if(nums.length < 4) {
+    for(int i=0; i<nums.length; i++) {
+      if(nums[i] == 9) {
+        return true;
+      }
+    }
+    return false;
+  }
+  for(int i=0; i<4; i++) {
+    if(nums[i] == 9) {
+      return true;
+    }
+  }
+  return false;
+}
