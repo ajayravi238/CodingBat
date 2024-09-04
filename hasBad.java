a2fe92fde7eec38ec548685e90e9b07180ef34cd
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-09-04 22:55:07 +0530

    Create hasBad

---
+++ hasBad.java
@@ -0,0 +1,12 @@
+public boolean hasBad(String str) {
+  if(str.length() < 3) {
+    return false;
+  }
+  else if(str.substring(0, 3).equals("bad")) {
+    return true;
+  }
+  else if(str.length() > 3 && str.substring(1, 4).equals("bad")) {
+    return true;
+  }
+  return false;
+}
