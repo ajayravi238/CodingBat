a36f2de3e0202cf50c6ee2075efd41f5801455c9
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-10-09 22:26:29 +0530

    Create modThree

---
+++ Array2/modThree.java
@@ -0,0 +1,11 @@
+public boolean modThree(int[] nums) {
+  for(int i=0; i<nums.length-2; i++) {
+    if(nums[i]%2 == 0 && nums[i+1]%2 == 0 && nums[i+2]%2 == 0) {
+      return true;
+    }
+    else if(nums[i]%2 != 0 && nums[i+1]%2 != 0 && nums[i+2]%2 != 0) {
+      return true;
+    }
+  }
+  return false;
+}
